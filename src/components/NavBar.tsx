009d957c5c81054b3cd475e131b1d4ac71d66483
Muaz <you@example.com>
2023-06-18 14:05:20 +0500

    Adding a button in navbar for search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,35 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image, useBreakpointValue } from "@chakra-ui/react";
 import logo from "../assets/webpack.svg";
 import ColorChanger from "./ColorChanger";
 import Search from "./Search";
+import ButtonForCard from "./buttonforCard";
 
 export interface SearchProps {
   setSearch: (search: string) => void;
 }
 
 const NavBar = ({ setSearch }: SearchProps) => {
+  const isSmallScreen = useBreakpointValue({
+    sm: false,
+    base: true,
+  });
+
+  if (isSmallScreen) {
+    // Render small screen version
+    return (
+      <>
+        <HStack justifyContent={"space-between"} padding={"10px 20px"}>
+          <Image src={logo} boxSize={"60px"} />
+          <HStack>
+            <ButtonForCard onClick={setSearch} />
+            <ColorChanger />
+          </HStack>
+        </HStack>
+      </>
+    );
+  }
+
+  // Render large screen version
   return (
     <HStack justifyContent={"space-between"} padding={"10px 20px"}>
       <Image src={logo} boxSize={"60px"} />
@@ -16,5 +38,15 @@ const NavBar = ({ setSearch }: SearchProps) => {
     </HStack>
   );
 };
+
+// const NavBar = ({ setSearch }: SearchProps) => {
+//   return (
+//     <HStack justifyContent={"space-between"} padding={"10px 20px"}>
+//       <Image src={logo} boxSize={"60px"} />
+//       <Search onSearch={setSearch} />
+//       <ColorChanger />
+//     </HStack>
+//   );
+// };
 
 export default NavBar;
