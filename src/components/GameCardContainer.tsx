499fe1587c4e170075b04e43185878c0b1a92107
Muaz <you@example.com>
2023-06-18 15:25:14 +0500

    adding  border Width

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderRadius={10} overflow={"hidden"}>
+    <Box borderRadius={10} overflow={"hidden"} borderWidth="1px">
       {children}
     </Box>
   );
