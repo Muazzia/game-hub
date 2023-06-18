8bcc68b5da24ac7d4daacb52a626796ef6e4262f
Muaz <you@example.com>
2023-06-18 11:38:03 +0500

    cleaning games

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   Spinner,
   Button,
+  Heading,
 } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import CropUrl from "../services/Image-Url";
@@ -23,28 +24,34 @@ const GenreList = ({ selected, onSelect }: Props) => {
   if (Error) return null;
   if (isloading) return <Spinner />;
   return (
-    <List>
-      {data.map((val) => (
-        <ListItem key={val.id} paddingY={2}>
-          <HStack key={val.id}>
-            <Image
-              boxSize={"28px"}
-              src={CropUrl(val.image_background)}
-              borderRadius={4}
-            />
-            <Button
-              onClick={() => onSelect(val)}
-              variant={"link"}
-              colorScheme={
-                selected ? (selected === val ? "blue" : "white") : "white"
-              }
-            >
-              {val.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize={"3xl"} marginBottom={2}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((val) => (
+          <ListItem key={val.id} paddingY={2}>
+            <HStack key={val.id}>
+              <Image
+                boxSize={"28px"}
+                src={CropUrl(val.image_background)}
+                borderRadius={4}
+                fit={"cover"}
+              />
+              <Button
+                onClick={() => onSelect(val)}
+                variant={"link"}
+                colorScheme={
+                  selected ? (selected === val ? "blue" : "white") : "white"
+                }
+              >
+                {val.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
